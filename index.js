baf86543efa104fbd62fe98d7dbb0016bd49a026
Mk Asik <mkasik123@gmail.com>
2022-11-10 18:24:20 +0600

    added services id

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
             const result = await serviceCollection.insertOne(service);
             res.send(result);
         });
-
+        // get data by id from serviecs
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
